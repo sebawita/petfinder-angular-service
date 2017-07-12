9aec71a603a6de88e223259aa0b6dcf4c640633e
sebawita <sebawita@gmail.com>
2017-07-12 12:48:47 +0100

    Making the PetFinderFactory AoT friendly

--- models/pet-finder-factory.ts
+++ models/pet-finder-factory.ts
@@ -1,18 +1,23 @@
 import { Pet, Media, Contact, Shelter } from './';
+import { RawPet, $t } from './raw-pet.model';
+
+// export function mapBreeds(rawPet) {
+//     return rawPet.breeds.breed.map(breed => breed.$t);
+// }
 
 export class PetFinderFactory {
-    static petFromRaw(rawPet: any): Pet {
+    static petFromRaw(rawPet: RawPet): Pet {
         return new Pet(
             rawPet.id.$t !== undefined ? rawPet.id.$t : undefined,
             rawPet.name.$t !== undefined ? rawPet.name.$t : undefined,
             rawPet.animal.$t !== undefined ? rawPet.animal.$t : undefined,
-            Array.isArray(rawPet.breeds.breed) ? rawPet.breeds.breed.map(breed => breed.$t) : rawPet.breeds.breed !== undefined ? [rawPet.breeds.breed.$t] : undefined,
-            rawPet.mix.$t !== undefined ? rawPet.mix == 'yes' : undefined,
+            PetFinderFactory.extractBreeds(rawPet),
+            rawPet.mix.$t !== undefined ? rawPet.mix.$t == 'yes' : undefined,
             rawPet.age.$t !== undefined ? rawPet.age.$t : undefined,
             rawPet.sex.$t !== undefined ? rawPet.sex.$t : undefined,
             rawPet.size.$t !== undefined ? rawPet.size.$t : undefined,
             rawPet.description.$t !== undefined ? rawPet.description.$t : undefined,
-            Array.isArray(rawPet.options.option) ? rawPet.options.option.map(option => option.$t) : rawPet.options.option !== undefined ? [rawPet.options.option.$t] : undefined ,
+            PetFinderFactory.extractOptions(rawPet),
             rawPet.status.$t !== undefined ? rawPet.status.$t : undefined,
             rawPet.lastUpdate.$t !== undefined ? new Date(rawPet.lastUpdate.$t) : undefined,
             rawPet.media.photos !== undefined ? PetFinderFactory.mediaFromRaw(rawPet.media) : PetFinderFactory.mediaFromEmpty(),
@@ -20,6 +25,35 @@ export class PetFinderFactory {
             rawPet.shelterId.$t !== undefined ? rawPet.shelterId.$t : undefined,
             rawPet.shelterPetId.$t !== undefined ? rawPet.shelterPetId.$t : undefined,
         );
+    }
+
+    /// AoT friendly version of
+    /// Array.isArray(rawPet.breeds.breed) ? rawPet.breeds.breed.map(breed => breed.$t) : rawPet.breeds.breed !== undefined ? [rawPet.breeds.breed.$t] : undefined,
+    static extractBreeds(pet: RawPet): string[] {
+        // return Array.isArray(pet.breeds.breed) ? pet.breeds.breed.map(breed => breed.$t) : pet.breeds.breed !== undefined ? [pet.breeds.breed.$t] : undefined;
+        if (Array.isArray(pet.breeds.breed)) {
+            return pet.breeds.breed.map(breed => breed.$t);
+        }
+
+        if (pet.breeds.breed !== null) {
+            return [pet.breeds.breed.$t];
+        }
+
+        return [];
+    }
+
+    /// Aot friendly version of
+    /// Array.isArray(rawPet.options.option) ? rawPet.options.option.map(option => option.$t) : rawPet.options.option !== undefined ? [rawPet.options.option.$t] : undefined ,
+    static extractOptions(pet: RawPet): string[] {
+        if (Array.isArray(pet.options.option)) {
+            return pet.options.option.map(option => option.$t);
+        }
+
+        if (pet.options.option !== null) {
+            return [pet.options.option.$t];
+        }
+
+        return [];
     }
 
     static contactFromRaw(rawContact: any): Contact {
