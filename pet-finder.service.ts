abf2751930dcdd94786ea7f0b71ccb2a9b7d08bf
sebawita <sebawita@gmail.com>
2017-07-12 12:56:29 +0100

    (Chore) fixing lint issues

--- pet-finder.service.ts
+++ pet-finder.service.ts
@@ -7,24 +7,24 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise'
 
-export const API_KEY_TOKEN = new InjectionToken<string>('api_key'); 
+export const API_KEY_TOKEN = new InjectionToken<string>('api_key');
 
 @Injectable()
 export class PetFinderService {
   private baseUrl = 'https://api.petfinder.com/';
 
-  constructor(@Inject(forwardRef(() => Http)) private http: Http, @Inject(API_KEY_TOKEN) private apiKey:string) {
+  constructor(@Inject(forwardRef(() => Http)) private http: Http, @Inject(API_KEY_TOKEN) private apiKey: string) {
   }
 
-  /** 
+  /**
    * Returns a list of breeds for a particular animal.
    * @param animal type of animal (barnyard, bird, cat, dog, horse, pig, reptile, smallfurry): for a safe list values use Options.animal
    */
-  public breedList(animal: string): Promise<Array<string>> {
+  public breedList(animal: string): Promise<string[]> {
     const requiredParams = { animal };
 
     return this.callPetFinder('breed.list', requiredParams)
-    .map(petfinder => 
+    .map(petfinder =>
       petfinder.breeds.breed
       .map(breed => breed.$t)
     )
@@ -33,7 +33,7 @@ export class PetFinderService {
 
   /**
    * Returns a record for a single pet.
-   * @param id 
+   * @param id
    */
   public getPet(id: string | number): Promise<Pet> {
     return this.callPetFinder('pet.get', {id})
@@ -42,12 +42,13 @@ export class PetFinderService {
   }
 
   /**
-   * Returns an id for a randomly selected pet. You can choose the characteristics of the pet you want returned using the various arguments to this method.
+   * Returns an id for a randomly selected pet.
+   * You can choose the characteristics of the pet you want returned using the various arguments to this method.
    * @param options a set of Random Search Options, which include: animal, breed, location, sex, shelterId, size
    */
   public getRandomPetId(options: RandomSearchOptions = {}): Promise<number> {
-    const requiredParams = { 
-      output: Options.output.id 
+    const requiredParams = {
+      output: Options.output.id
     };
 
     return this.callPetFinder('pet.getRandom', requiredParams, options)
@@ -56,7 +57,8 @@ export class PetFinderService {
   }
 
   /**
-   * Returns a record for a randomly selected pet. You can choose the characteristics of the pet you want returned using the various arguments to this method.
+   * Returns a record for a randomly selected pet.
+   * You can choose the characteristics of the pet you want returned using the various arguments to this method.
    * @param options a set of Search Options, which include: animal, breed, location, sex, shelterId, size
    * @param provideDescription determines whether the pet description should be provided
    */
@@ -77,7 +79,7 @@ export class PetFinderService {
    * @param location the ZIP/postal code or city and state the animal should be located (NOTE: the closest possible animal will be selected)
    * @param options a set of Search Options, which include: age, animal, breed, count, offset, output, sex, shelterId, size
    */
-  public findPets(location: string, options: PetSearchOptions = {}): Promise<Array<Pet>> {
+  public findPets(location: string, options: PetSearchOptions = {}): Promise<Pet[]> {
     const requiredParams = { location };
 
     return this.callPetFinder('pet.find', requiredParams, options)
@@ -89,13 +91,13 @@ export class PetFinderService {
     })
     .toPromise();
   }
-  
+
   /**
    * Returns a list of pet records for an individual shelter.
    * @param id shelter ID (e.g. NJ94)
    * @param options a set of Search Options, which include: count, offset, output, status
    */
-  public findShelterPets(id: string | number, options: ShelterPetSearchOptions = {}): Promise<Array<Pet>> {
+  public findShelterPets(id: string | number, options: ShelterPetSearchOptions = {}): Promise<Pet[]> {
     const requiredParams = { id };
 
     return this.callPetFinder('shelter.getPets', requiredParams, options)
@@ -114,7 +116,7 @@ export class PetFinderService {
    * @param location the ZIP/postal code or city and state where the search should begin
    * @param options a set of Search Options, which include: count, name, offset
    */
-  public findShelters(location: string, options: ShelterSearchOptions = {}): Promise<Array<Shelter>> {
+  public findShelters(location: string, options: ShelterSearchOptions = {}): Promise<Shelter[]> {
     const requiredParams = { location };
 
     return this.callPetFinder('shelter.find', requiredParams, options)
@@ -143,11 +145,12 @@ export class PetFinderService {
   /**
    * Returns a list of shelters, listing animals of a particular breed.
    * WARNING: Shelter name is not returned!
-   * @param animal type of animal, valid values: barnyard, bird, cat, dog, horse, pig, reptile, smallfurry. For a safe list of values use Options.animal
+   * @param animal type of animal, valid values: barnyard, bird, cat, dog, horse, pig, reptile, smallfurry.
+   * For a safe list of values use Options.animal
    * @param breed breed of animal, use breedList() for a list of valid breeds
-   * @param options a set of Search Options, which include: count, offset 
+   * @param options a set of Search Options, which include: count, offset
    */
-  public findSheltersByBreed(animal:string, breed: string, options: ShelterSearchByBreedOptions = {}): Promise<Array<Shelter>> {
+  public findSheltersByBreed(animal:string, breed: string, options: ShelterSearchByBreedOptions = {}): Promise<Shelter[]> {
     const requiredParams = { animal, breed };
 
     return this.callPetFinder('shelter.listByBreed', requiredParams, options)
@@ -168,7 +171,7 @@ export class PetFinderService {
    * @param options an object containing optional parameters
    */
   private callPetFinder(method: string, params: any = {}, options: any = {}): Observable<any> {
-    let searchParams: URLSearchParams = this.buildSearchParams(params, options);
+    const searchParams: URLSearchParams = this.buildSearchParams(params, options);
     return this.http.get(
       // `${this.baseUrl}${method}`,
       this.baseUrl + method,
@@ -183,22 +186,22 @@ export class PetFinderService {
       }
     })
   }
-  
+
   /**
    * Constructs an http ready set of parameters based on the provided required and optional parameters.
    * @param params an object containing the required parameters
    * @param options an object containing optional parameters
    */
   private buildSearchParams(params: any, options: any) {
-    let searchParams: URLSearchParams = new URLSearchParams();
+    const searchParams: URLSearchParams = new URLSearchParams();
     searchParams.set('key', this.apiKey);
     searchParams.set('format', 'json');
 
-    for (let key in params) {
+    for (const key in params) {
       searchParams.set(key, params[key]);
     }
 
-    for (let key in options)  {
+    for (const key in options)  {
       searchParams.set(key, options[key]);
     }
 
